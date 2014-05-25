16ef1f0fb4527d48a7822e23f184d8cadd5fcec3
Michael Rosenberger <leahcim165@gmail.com>
2014-05-24 18:33:47 -0700

    Even more tables.

--- alters/2014-05-24-000-initial-table-structure.sql
+++ alters/2014-05-24-000-initial-table-structure.sql
@@ -6,49 +6,58 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS `question`;
 CREATE TABLE `question` (
-	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `unit_type_id` INT(10) UNSIGNED NOT NULL, 
-	`prompt` VARCHAR(256) NOT NULL,
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Primary key.',
+  `unit_type_id` INT(10) UNSIGNED NOT NULL COMMENT 'Type of unit that response should be in (mass, time, etc.).', 
+  `default_unit_id` INT(10) UNSIGNED NOT NULL COMMENT 'Unit that question and display should default to.',
+	`prompt` VARCHAR(256) NOT NULL COMMENT 'Question to ask the user.',
 	PRIMARY KEY (`id`),
-  FOREIGN KEY (`unit_type_id`) REFERENCES `unit_type` (`id`)
+  FOREIGN KEY (`unit_type_id`) REFERENCES `unit_type` (`id`),
+  FOREIGN KEY (`default_unit_id`) REFERENCES `unit` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `answer`;
 CREATE TABLE `answer` (
-	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `question_id` INT(10) UNSIGNED NOT NULL,
-  `user_id` INT(10) UNSIGNED NOT NULL,
-  `value` DOUBLE NOT NULL,
-  `answered_at` TIMESTAMP NOT NULL,
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Primary key.',
+  `question_id` INT(10) UNSIGNED NOT NULL COMMENT 'Reference to question that this is an answer to.',
+  `user_id` INT(10) UNSIGNED NOT NULL COMMENT 'Reference to user who answered the question.',
+  `unit_id` INT(10) UNSIGNED NOT NULL COMMENT 'Unit that answer is in.',
+  `value` DOUBLE NOT NULL COMMENT 'Numeric value of the response.',
+  `answered_at` TIMESTAMP NOT NULL 'Timestamp at which the answer was created.',
   PRIMARY KEY (`id`),
   FOREIGN KEY (`question_id`) REFERENCES `question` (`id`),
-  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  FOREIGN KEY (`unit_id`) REFERENCES `unit` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `unit`;
 CREATE TABLE `unit` (
 	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `unit_type_id` INT(10) UNSIGNED,
-  `value_in_canonical_unit` DOUBLE NOT NULL,
-  `canonical` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+  `value_in_canonical_unit` DOUBLE NOT NULL COMMENT 'Conversion factor from canonical unit (for this unit_type) to this unit.',
+  `canonical_p` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' COMMENT '1 if this is the canonical unit for this unit_type, 0 otherwise.',
+  `name_singular` VARCHAR(256) NOT NULL COMMENT 'Singular version of name.',
+  `name_plural` VARCHAR(256) NOT NULL COMMENT 'Plural version of name.',
+  `abbreviation` VARCHAR(256) NOT NULL COMMENT 'Abbreviation of unit.',
+  `display_p` TINYINT(1) UNSIGNED NOT NULL DEFAULT '1' COMMENT '1 if should be displayed to users, 0 otherwise.',
   PRIMARY KEY (`id`),
   FOREIGN KEY (`unit_type_id`) REFERENCES `unit_type` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `unit_type`;
 CREATE TABLE `unit_type` (
-	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(256) NOT NULL,
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Primary key.',
+  `name` VARCHAR(256) NOT NULL COMMENT 'Name of unit type (mass, etc.).',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(256) NOT NULL, 
-  `first_name` VARCHAR(256) NOT NULL,
-  `last_name` VARCHAR(256) NOT NULL,
-  `hashed_password` VARCHAR(256) NOT NULL,
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Primary key.',
+  `email` VARCHAR(512) NOT NULL COMMENT 'User email.', 
+  `first_name` VARCHAR(256) NOT NULL COMMENT 'User first name.',
+  `last_name` VARCHAR(256) NOT NULL COMMENT 'User last name.',
+  `hashed_password` VARCHAR(256) NOT NULL COMMENT 'Salted hashed user password.',
+  `signup_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Timestamp of user signup.'
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- alters/2014-05-24-001-initial-unit-values.sql
+++ alters/2014-05-24-001-initial-unit-values.sql
@@ -4,6 +4,42 @@ SET AUTOCOMMIT = 0;
 SET FOREIGN_KEY_CHECKS = 0;
 
 TRUNCATE TABLE unit;
+TRUNCATE TABLE unit_type;
+
+INSERT INTO unit_type (`name`) VALUES
+  ('quantity'),
+  ('time'),
+  ('length'),
+  ('mass'),
+  ('temperature'),
+  ('volume'),
+  ('area'),
+  ('energy');
+
+INSERT INTO unit (`unit_type_id`, `value_in_canonical_unit`, `canonical_p`, `name_singular`, `name_plural`, `abbreviation`) VALUES
+  ((SELECT id FROM unit_type WHERE name='time'), 1.0, 1, 'second', 'seconds', 'S'),
+  ((SELECT id FROM unit_type WHERE name='time'), 60, 0, 'minute', 'minutes', 'M'),
+  ((SELECT id FROM unit_type WHERE name='time'), 3600, 0, 'hour', 'hours', 'H'),
+
+  ((SELECT id FROM unit_type WHERE name='mass'), 1.0, 1, 'kilogram', 'kilograms', 'kg'),
+  ((SELECT id FROM unit_type WHERE name='mass'), 0.001, 0, 'gram', 'grams', 'g'),
+  ((SELECT id FROM unit_type WHERE name='mass'), 0.453592, 0, 'pound', 'pounds', 'lb'),
+  ((SELECT id FROM unit_type WHERE name='mass'), 0.0283495, 0, 'ounce', 'ounces', 'oz'),
+
+  ((SELECT id FROM unit_type WHERE name='length'), 1.0, 1, 'meter', 'meters', 'm'),
+  ((SELECT id FROM unit_type WHERE name='length'), 0.01, 0, 'centimeter', 'centimeters', 'cm'),
+  ((SELECT id FROM unit_type WHERE name='length'), 0.001, 0, 'millimeter', 'millimeters', 'mm'),
+  ((SELECT id FROM unit_type WHERE name='length'), 0.0254, 0, 'inch', 'inches', 'in'),
+  ((SELECT id FROM unit_type WHERE name='length'), 0.3048, 0, 'foot', 'feet', 'ft'),
+
+  ((SELECT id FROM unit_type WHERE name='volume'), 1.0, 1, 'liter', 'liters', 'L'),
+  ((SELECT id FROM unit_type WHERE name='volume'), 0.001, 0, 'milliliter', 'milliliters', 'mL'),
+  ((SELECT id FROM unit_type WHERE name='volume'), 0.0295735, 0, 'fluid ounce', 'fluid ounces', 'fl. oz.'),
+  ((SELECT id FROM unit_type WHERE name='volume'), 0.236588, 0, 'cup', 'cups', 'cup'),
+
+  ((SELECT id FROM unit_type WHERE name='energy'), 1.0, 1, 'kilocalorie', 'kilocalories', 'kcal'),
+
+  ((SELECT id FROM unit_type WHERE name='temperature'), 1.0, 1, 'degree Celsius', 'degrees Celcius', '&deg;C');
 
 SET AUTOCOMMIT = 1;
 SET FOREIGN_KEY_CHECKS = 1;
