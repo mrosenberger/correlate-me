af2a075e1a29b47ddd873f84c9860b22684b53e8
Michael Rosenberger <leahcim165@gmail.com>
2014-05-25 00:52:36 -0700

    Even more huge changes.

--- node-app/node-main.js
+++ node-app/node-main.js
@@ -2,7 +2,9 @@ var express = require("express"),
     cookie = require("cookie-parser"),
     session = require("express-session"),
     bodyParser = require("body-parser"),
-    ejs = require("ejs");
+    ejs = require("ejs"),
+    underscore = require("underscore"),
+    path = require("path");
 
 var app = express();
 
@@ -12,21 +14,37 @@ app.use(cookie());
 app.use(session({secret: "sessionsecret512234"}));
 app.use(bodyParser());
 
+app.use('/public', express.static(path.join(__dirname, 'vendor/thoughtbrain')));
+
 app.set("views", __dirname + "/templates");
 
-app.use("/stylesheets", express.static(__dirname + "/assets/stylesheets"));
-app.use("/scripts", express.static(__dirname + "/assets/scripts"));
-app.use("/images", express.static(__dirname + "/assets/images"));
+app.use("/stylesheets", express.static(path.join(__dirname, "assets/stylesheets")));
+app.use("/scripts", express.static(path.join(__dirname, "assets/scripts")));
+app.use("/images", express.static(path.join(__dirname, "assets/images")));
 
 app.get("/", function(req, res) {
-  req.session.user_id = 65332;
-  res.send("Welcome to CorrelateMe!");
+  res.render("index");
+});
 
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.get("/sign-up", function(req, res) {
+  res.render("sign-up");
+});
+
+app.get("/sign-in", function(req, res) {
+  res.render("sign-in");
 });
 
 app.get("/template-test", function(req, res) {
   res.render("myview");
-})
+});
+
+app.post("/perform-sign-up", function(req, res) {
+  console.log("performing sign up!");
+});
 
 app.get("/signed-in", function(req, res) {
   if (req.session.user_id) {
